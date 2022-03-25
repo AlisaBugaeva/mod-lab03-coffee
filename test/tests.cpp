c5e009f5db3ea254c25ceeed43fc14dd2cd86611
AlisaBugaeva <92516286+AlisaBugaeva@users.noreply.github.com>
2022-03-25 18:03:55 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -33,7 +33,7 @@ TEST(test2, offState) {
       a1.on();
       a1.off();
       STATES result = a1.getState();
-      EXPECT_EQ(WAIT, result);
+      EXPECT_EQ(OFF, result);
 }
 
 TEST(test3, acceptState) {
@@ -48,6 +48,40 @@ TEST(test3, acceptState) {
       Automata a1(menu, prices);
       a1.on();
       a1.coin(10);
+      STATES result = a1.getState();
+      EXPECT_EQ(ACCEPT, result);
+}
+
+TEST(test4, cancel) {
+      std::string menu[5] = {
+        "black tea",
+        "green tea",
+        "cappuccino",
+        "latte",
+        "hot chocolate"
+     };
+      int prices[5] = { 90, 90, 120, 130, 100 };
+      Automata a1(menu, prices);
+      a1.on();
+      a1.coin(10);
+      a1.cancel();
+      STATES result = a1.getState();
+      EXPECT_EQ(WAIT, result);
+}
+
+TEST(test5, notEnoughMoney) {
+      std::string menu[5] = {
+        "black tea",
+        "green tea",
+        "cappuccino",
+        "latte",
+        "hot chocolate"
+     };
+      int prices[5] = { 90, 90, 120, 130, 100 };
+      Automata a1(menu, prices);
+      a1.on();
+      a1.coin(10);
+      a1.choice(2);
       STATES result = a1.getState();
       EXPECT_EQ(WAIT, result);
 }
