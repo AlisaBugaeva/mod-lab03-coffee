64e447844058537a87b70aaf9bfd4650415abde2
AlisaBugaeva <alisabugaeva@mail.ru>
2022-03-25 17:05:34 +0300

    new

--- include/Automata.h
+++ include/Automata.h
@@ -1,1 +1,26 @@
+#pragma once
+#include <iostream>
+#include <string>
 
+enum STATES { COOK, CHECK, ACCEPT, WAIT, OFF };
+
+class Automata {
+public:
+    void on(); // включение автомата;
+    void off(); // выключение автомата;
+    void coin(int coins); // занесение денег на счёт пользователем;
+    void getMenu(); // считывание меню с напитками и ценами для пользователя;
+    void getState(); // считывание текущего состояния для пользователя;
+    void choice(int drinkNumber); // выбор напитка пользователем;
+    void check(int drinkNumber); // проверка наличия необходимой суммы;
+    void cancel(); // отмена сеанса обслуживания пользователем;
+    void cook(); // имитация процесса приготовления напитка;
+    void finish(); // завершение обслуживания пользователя.
+    Automata(std::string* menu, int* prices); //конструктор
+
+private:
+    double cash; // для хранения текущей суммы;
+    std::string menu[5]; // массив строк названий напитков;
+    int prices[5]; // массив цен напитков;
+    STATES state; // текущее состояние автомата;
+};
---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iostream>
+#include "Automata.h"
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+void main() {
+    std::string menu[5] = { "black tea" , "green tea", "cappuccino", "latte", "hot chocolate" };
+    int prices[5] = { 90, 90, 120, 130, 100 };
+    int customerChoice;
+
+    Automata a1 ( menu, prices);
+
+    a1.on();
+    a1.coin(100);
+    cin >> customerChoice;
+    a1.choice(customerChoice);
+    a1.off();
+}
